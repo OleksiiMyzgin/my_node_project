0263815d40e96f6d97ad291dc76a0345ad50a71e
Krulatuy <krulatuy@gmail.com>
2017-10-13 20:39:25 +0300

    secont module core concept - Middleware and Error Handling

--- routes/index.js
+++ routes/index.js
@@ -5,28 +5,7 @@ const storeController = require('../controllers/storeController');
 // req is object full of info that coming in
 // res is object full of methods that sending data back to user 
 // Do work here
-// router.get('/', (req, res) => {
-//     const alex = { name: 'alex', age: 100, cool: true };
-//     // res.send('Hey! It works!');
-//     // res.json(alex);
-//     // res.send(req.query.name);
-//     // res.json(req.query);
-//     // res.render('hello', {
-//     //     name: 'alex',
-//     //     age: 100
-//     // });
-//     res.render('hello', {
-//         title: 'I love Food'
-//     });
-// });
 
-// router.get('/reverse/:name', (req, res) => {
-//     // const reverse = [...req.params.name]; // ['a','l','e','x','e','y']
-//     // const reverse = [req.params.name]; // ['alexey']
-//     const reverse = [...req.params.name].reverse().join('');
-//     res.send(reverse);
-// });
-
-router.get('/', storeController.homePage)
+router.get('/', storeController.myMiddleware, storeController.homePage)
 
 module.exports = router;
