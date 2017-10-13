cba0fe8844e79e7f3d74bf6c852aa5e77cfd3809
Krulatuy <krulatuy@gmail.com>
2017-10-13 12:14:54 +0300

    secont module core concept - Routing

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+// req is object full of info that coming in
+// res is object full of methods that sending data back to user 
+// Do work here
+router.get('/', (req, res) => {
+    const alex = { name: 'alex', age: 100, cool: true };
+    // res.send('Hey! It works!');
+    // res.json(alex);
+    // res.send(req.query.name);
+    res.json(req.query);
+});
+
+router.get('/reverse/:name', (req, res) => {
+    // const reverse = [...req.params.name]; // ['a','l','e','x','e','y']
+    // const reverse = [req.params.name]; // ['alexey']
+    const reverse = [...req.params.name].reverse().join('');
+    res.send(reverse);
+});
+
+module.exports = router;
