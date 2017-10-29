44ee31063f986ffd9aaa7af8c2701f20841ec824
Krulatuy <krulatuy@gmail.com>
2017-10-29 15:07:57 +0200

    Sending email with Nodejs

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const promisify = require('es6-promisify');
+const mail = require('../handlers/mail');
 
 
 exports.login = passport.authenticate('local', {
@@ -41,7 +42,13 @@ exports.forgot = async(req, res) => {
     await user.save();
     // 3. Send them an email with the token
     const resetURL = `https://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
-    req.flash('success', `You have been emailed a password reset link. ${resetURL}`);
+    await mail.send({
+        user,
+        subject: 'Password Reset',
+        resetURL, //https://localhost:7777/account/reset/0d63d7f5cd68053b26d61bdcd5101e8118b3a896
+        filename: 'password-reset',
+    });
+    req.flash('success', `You have been emailed a password reset link.`);
     // 4. redirect to login page
     res.redirect('/login');
 };
