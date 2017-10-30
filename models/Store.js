c466a05681428f31eadb214d6f50ebc0f2cb2b7d
Krulatuy <krulatuy@gmail.com>
2017-10-30 12:33:38 +0200

    Locking down aor application with User Permissions

--- models/Store.js
+++ models/Store.js
@@ -32,7 +32,12 @@
           required: 'You must supply an address!'
       },
   },
-  photo: String
+  photo: String,
+  author: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: 'You must supply an author'
+  }
  });
 
  storeSchema.pre('save', async function(next) {
